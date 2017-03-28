50644db2f324bf5620458a83fafc5e32e751db2b
parksjr <mike@parksjr.tech>
2017-03-28 12:24:02 -0400

    added postgrator migration helper features

---
+++ pgtemplates/pg.undo.crud.sql
@@ -0,0 +1,18 @@
+-- ================================================
+-- Template generated from the mo-cli-tool for 
+-- postgrator. https://github.com/parksjr/mo-cli-tool
+--
+-- This file contains the undo actions for the crud
+-- template.
+--
+-- Make the necessary changes to fulfill your needs.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+
+drop procedure <ProcedureName>_createOne
+drop procedure <ProcedureName>_getOne
+drop procedure <ProcedureName>_getAll
+drop procedure <ProcedureName>_updateOne
+drop procedure <ProcedureName>_deleteOne
---
+++ pgtemplates/pg.undo.procedure.sql
@@ -0,0 +1,14 @@
+-- ================================================
+-- Template generated from the mo-cli-tool for 
+-- postgrator. https://github.com/parksjr/mo-cli-tool
+--
+-- This file contains the undo actions for the 
+-- procedure template.
+--
+-- Make the necessary changes to fulfill your needs.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+
+drop procedure <ProcedureName>
